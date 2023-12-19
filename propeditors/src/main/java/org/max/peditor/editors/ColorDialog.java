a4b973b78ac6e5c16bb4ed30f975f9e99de763a1
CaptainVoronin <CaptainVoronin@gmail.com>
2023-12-19 13:50:57 +0300

    Заменен диалог выбора цветов

---
+++ propeditors/src/main/java/org/max/peditor/editors/ColorDialog.java
@@ -0,0 +1,106 @@
+package org.max.peditor.editors;
+
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.GradientDrawable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AlertDialog;
+
+import com.madrapps.pikolo.ColorPicker;
+import com.madrapps.pikolo.listeners.SimpleColorSelectionListener;
+
+import org.max.peditor.R;
+
+public class ColorDialog {
+    private final Context context;
+    private String title;
+    private int color;
+    private OnClickListener positiveListener;
+
+    AlertDialog dialog;
+
+    ImageView imageView;
+
+    Button colorButton;
+
+    GradientDrawable background;
+
+    public ColorDialog(Context context) {
+        this.context = context;
+    }
+
+    public ColorDialog setTitle(String title) {
+        this.title = title;
+        return this;
+    }
+
+    public ColorDialog setSelectedColor(int color) {
+        this.color = color;
+        return this;
+    }
+
+    public ColorDialog show() {
+        dialog = build();
+        dialog.show();
+        return this;
+    }
+
+    private AlertDialog build() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        if (title != null)
+            builder.setTitle(title);
+        builder.setView(getView());
+        builder.setNegativeButton(android.R.string.cancel, (dialog, i) -> dialog.cancel());
+        builder.setPositiveButton(android.R.string.ok, (dialog, i) -> {
+            if (positiveListener != null)
+                positiveListener.onButtonClick(getSelectedColor());
+            dialog.cancel();
+        });
+
+        return builder.create();
+    }
+
+    private View getView() {
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View view = inflater.inflate(org.max.peditor.R.layout.color_dialog, null);
+        ColorPicker colorPicker = view.findViewById(org.max.peditor.R.id.colorPicker);
+        colorPicker.setColor(color);
+        imageView = view.findViewById(R.id.imageView);
+        background = (GradientDrawable) imageView.getBackground();
+        colorButton = view.findViewById(R.id.randomColorButton);
+        innerSetColor( color );
+        colorPicker.setColorSelectionListener(new SimpleColorSelectionListener() {
+            @Override
+            public void onColorSelected(int color) {
+                ColorDialog.this.color = color;
+                innerSetColor(color);
+            }
+        });
+        return view;
+    }
+
+    public ColorDialog setPositiveButtonListener(OnClickListener listener) {
+        this.positiveListener = listener;
+        return this;
+    }
+
+    public interface OnClickListener {
+        void onButtonClick(int selectedColor);
+    }
+
+    public int getSelectedColor() {
+        return color;
+    }
+
+    private void innerSetColor(int c) {
+
+        background.setColor(c);
+        colorButton.setBackgroundColor(c);
+        String hexColor = String.format("#%06X", (0xFFFFFF & c));
+        colorButton.setText(hexColor);
+    }
+}
--- propeditors/src/main/java/org/max/peditor/editors/PropertyEditorColor.java
+++ propeditors/src/main/java/org/max/peditor/editors/PropertyEditorColor.java
@@ -3,12 +3,6 @@ package org.max.peditor.editors;
 import android.content.Context;
 import android.graphics.Color;
 
-import com.github.dhaval2404.colorpicker.ColorPickerDialog;
-import com.github.dhaval2404.colorpicker.MaterialColorPickerDialog;
-import com.github.dhaval2404.colorpicker.listener.ColorListener;
-import com.github.dhaval2404.colorpicker.model.ColorShape;
-import com.github.dhaval2404.colorpicker.model.ColorSwatch;
-
 public class PropertyEditorColor extends APropertyEditor<Color> {
     public PropertyEditorColor(Context context, ITypeConverter<Color> converter) {
         super(context, converter);
@@ -16,28 +10,15 @@ public class PropertyEditorColor extends APropertyEditor<Color> {
 
     @Override
     public void show() {
-        MaterialColorPickerDialog.Builder dlg = new MaterialColorPickerDialog
-                .Builder(getContext())
-                .setTitle(getTitle())
-                .setColorSwatch(ColorSwatch._900)
-                .setColorShape(ColorShape.SQAURE);
-        if (getValue() != null)
-        {
-            String hex= String.format("#%06X", (0xFFFFFF & getValue().toArgb()));
-            dlg.setDefaultColor( hex );
-        }
-        else
-        {
-            dlg.setDefaultColor( "#FF000000" );
-        }
-
-        dlg.setColorListener((color, colorHex) -> {
-            if (getListener() != null)
-                getListener().onOkPressed(getTypeConverter().convertValue(Color.parseColor(colorHex)));
-
+        ColorDialog dialog = new ColorDialog(getContext());
+        dialog.setPositiveButtonListener( (color)->{
+            if( getListener()!= null )
+                getListener().onOkPressed( getTypeConverter().convertValue( color ) );
         });
-
-
-        dlg.show();
+        if( getValue()!= null )
+            dialog.setSelectedColor( getValue().toArgb() );
+        else
+            dialog.setSelectedColor( Color.BLACK );
+        dialog.show();
     }
 }
